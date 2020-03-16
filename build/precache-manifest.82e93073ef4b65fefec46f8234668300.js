aa9440e19c63895178cf8fde41c4d453f9670782
soycoder <om.apisith@gmail.com>
2020-03-16 18:08:45 +0700

    Create a React app and publish it to GitHub Pages

---
+++ build/precache-manifest.82e93073ef4b65fefec46f8234668300.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5602d2b0d49c8ee2ec701ad4a39d6139",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "117dc796dcbd4e466502",
+    "url": "/my-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ddda087d77853d7825b7",
+    "url": "/my-react-app/static/js/2.eb6cc1f6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-react-app/static/js/2.eb6cc1f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "117dc796dcbd4e466502",
+    "url": "/my-react-app/static/js/main.4258cfd3.chunk.js"
+  },
+  {
+    "revision": "d6221124d7df5fc3d89f",
+    "url": "/my-react-app/static/js/runtime-main.a13eb2fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
